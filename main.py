3cb0a9b29e530299e2f14ffb2880b1ae0196fc5a
Lazyrdin <sarypovadiana1@gmail.com>
2024-12-22 22:28:59 +0700

    22.12.2024 Basic data structures 2

---
+++ main.py
@@ -0,0 +1,11 @@
+# 1st program
+print(9 ** 0.5 * 5)
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1 )
+# 3rd program
+print(2 * 2 + 2) # = 6
+print(2 * (2 + 2)) # = 8
+print(2 * 2 + 2 == 2 * (2 + 2)) # False
+# 4th program
+print(float('123.456') * 10) # 1234.56
+print(int(1234.56) % 10)
